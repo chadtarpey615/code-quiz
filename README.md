eab4581d03a1aef9c2da22430e5d07433f2f41ae
Chad Tarpey <chadtarpey@gmail.com>
2020-08-29 21:07:49 -0500

    Updated CSS, JavaScript and finshed my README

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
+# Chad Tarpey
+![Project Image](assets/codeQuiz.png)
+
+---
+
+### Table of Contents
+- [Description](#description)
+- [How To Use](#how-to-use)
+- [References](#references)
+- [License](#license)
+- [Author Info](#author-info)
+---
+
+---
+
 # code-quiz
-week-4-homework
+---
+## Acceptance Criteria
+```
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+```
+
+
+# Purpose
+
+A JavaScript Quiz!! This a multiple choice quiz to test your knowledge in this field.
+So go ahead and give it your best shot and see how much JavaScript you really know.
+Here's the scoring system
+* You have 60 seconds total to finish the quiz
+* If answered correctly you get 20 points.
+* If answered incorrectly you get no points and a 10 second reduction from the available time.
+* When finished with the quiz the time remaning gets added to your final score.
+
+# Built With these Technologies
+* HTML
+* CSS
+* BOOTSTRAP
+* JavaScript
+* VS Code
+
+
+
+## Description
+JavaScript Quiz!!
+
+---
+## How To Use
+- Visit  https://chadtarpey615.github.io/code-quiz/
+- Google Chrome or any Web Browser
+---
+## References
+- http://www.youtube.com
+- http://www.github.com
+- http://www.w3schools.com
+- Vanderbilt/Triolgy Coding Bootcamp
+---
+## License
+MIT License
+Copyright (c) [2020] [Chad M Tarpey]
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+---
+## Author Info
+- Email - [chadtarpey@gmail.com](chadtarpey@gmail.com)
+- Github - [github.com/hadtarpey615]
--- logic.js
+++ logic.js
@@ -2,7 +2,7 @@
 let timer = document.querySelector("#timer");
 let startbtn = document.querySelector("#start-quiz");
 let stopbtn = document.querySelector("#stop-quiz")
-
+let createUl = document.getElementById("answer")
 let newQuest = document.querySelector(".questions");
 let button1 = document.getElementById("answer1");
 let button2 = document.getElementById("answer2");
@@ -10,21 +10,30 @@ let button3 = document.getElementById("answer3");
 let button4 = document.getElementById("answer4");
 let showScore = document.getElementById("show-score");
 let highScores = document.getElementById("high-score");
+let scoreList = document.getElementById("scoreList")
+let scoreEnter = document.getElementById("scoreEnter");
+let mainForm = document.getElementById("main-Form");
+let buttonGroup = document.getElementById("buttonGroup");
+let correct = document.getElementById("correct");
+let inCorrect = document.getElementById("incorrect");
+let resetGame = document.getElementById("reset");
+let quizBody = document.getElementById("quizBody");
 
+let scoreText;
 let startingTime = 1;
 let totalSeconds = startingTime * 60;
 let interval;
-let questionsLeft = 4;
+let questionsLeft = 0;
 let score = 0;
-console.log(highScores);
-
+let finalScore;
+let totalScore = [];
 
 
 let questions = [{
 
-    question: "What is the difference between const and let in JavaScript",
-    choices: ["Nothing, they are the same", "you dont use const in JavaScript", "Const can not be reassigned while let can be reassigned"],
-    answer: "Const can not be reassigned while let can be reassigned"
+    question: "The let statement is used to?",
+    choices: ["Retrieve a variable descriptor", "Declare a member of a class", "Create a new local variable", "Change a constant"],
+    answer: "Create a new local variable"
 }, {
     question: "What is NaN in JavaScript?",
     choices: ["Not a Number", "undefined", "N/A", "No available Number"],
@@ -39,7 +48,7 @@ let questions = [{
     answer: "true/false statement"
 }, {
     question: "When you don't know what to do in JavaScript you should?",
-    choices: ["give up, run the code anyways", "use google to ge a better understanding", "give up"],
+    choices: ["give up, run the code anyways", "use google to ge a better understanding", "give up", "Call Mom"],
     answer: "use google to ge a better understanding"
 }];
 
@@ -48,119 +57,196 @@ button2.setAttribute("style", "display: none");
 button3.setAttribute("style", "display: none");
 button4.setAttribute("style", "display: none");
 showScore.setAttribute("style", "display: none");
+mainForm.setAttribute("style", "display: none");
 
 
-
-function startQuiz(event) {
-    event.preventDefault();
+function startQuiz() {
     setTimer();
-    randomQuestion();
-    newQuest.classList.add("hide");
+    buttonGroup.setAttribute("style", "display: none")
 
 
+    newQuestion();
+    newQuest.setAttribute("style", "display: block");
 
 
+    //newQuest.classList.add("hide");
 
 }
 
+function newQuestion() {
 
-function randomQuestion() {
+    if (questionsLeft < questions.length) {
+        newQuest.innerHTML = questions[questionsLeft].question;
+        button1.innerHTML = questions[questionsLeft].choices[0];
+        button2.innerHTML = questions[questionsLeft].choices[1];
+        button3.innerHTML = questions[questionsLeft].choices[2];
+        button4.innerHTML = questions[questionsLeft].choices[3];
 
+        button1.setAttribute("style", "display: inline-block");
+        button2.setAttribute("style", "display: inline-block");
+        button3.setAttribute("style", "display: inline-block");
+        button4.setAttribute("style", "display: inline-block");
+    } else {
 
-
-    newQuest.innerHTML = questions[questionsLeft].question;
-    button1.innerHTML = questions[questionsLeft].choices[0];
-    button2.innerHTML = questions[questionsLeft].choices[1];
-    button3.innerHTML = questions[questionsLeft].choices[2];
-    button4.innerHTML = questions[questionsLeft].choices[3];
-    button1.setAttribute("style", "display: inline-block");
-    button2.setAttribute("style", "display: inline-block");
-    button3.setAttribute("style", "display: inline-block");
-    button4.setAttribute("style", "display: inline-block");
-
-
-    // Tried using a forEach for my choices
-    //questions[questionsLeft].choices.forEach(function (choices) {
-    // let li = document.createElement("li");
-    //createUl.appendChild(li)
-
-    //li.innerHTML += choices + " <button id="+choices+">submit</button>"
-    //ea++;
-    //newQuest.classList.remove("hide")
-    //})
-
+        checkScore();
+    }
 };
 
 
-
+// get the next question
 function nextQuestion() {
     //console.log(this.textContent);
     if (this.textContent === questions[questionsLeft].answer) {
-        score += 20;
-    } else {
+        correct.setAttribute("style", "display: inline-block")
+       score += 20;
+
+    } else if (this.textContent !== questions[questionsLeft].answer) {
+        inCorrect.setAttribute("style", "display: inline-block")
+
         score -= 0;
         totalSeconds -= 10;
     }
-    questionsLeft--;
-    randomQuestion();
+
+    questionsLeft++;
+    
+    newQuestion();
 
 }
-
 
 //  set timer
 function setTimer() {
     const minutes = Math.floor(totalSeconds / 60);
     interval = setInterval(function () {
         timer.textContent = totalSeconds--;
+
         if (totalSeconds === 0) {
             alert("You ran out of time sorry please try again")
-        } else if (questionsLeft === -1) {
-            alert("Congrats!! You have finshed the quiz");
+
+        } else if (questionsLeft === 5) {
+
             clearInterval(interval);
             timer.textContent = score + totalSeconds;
+            finalScore = score + totalSeconds;
+
             showScore.setAttribute("style", "display: inline-block");
-            saveHighScore();
+
             stopTimer();
+
         }
     }, 1000)
 };
 
+init();
+
+function setScore() {
+    // Clear todoList element and update todoCountSpan
+    scoreList.innerHTML = "";
 
 
-function saveHighScore() {
-    highScores = JSON.parse(localStorage.getItem("highScores"));
+    // Render a new li for each name
+    for (var i = 0; i < totalScore.length; i++) {
+        var addScore = totalScore[i];
 
-     highScore = {
-        initials: prompt("Enter Your Initials to save your score"),
-        score: score + totalSeconds
-        //initials: userName.value
-    };
-    highScores.push(highScore);
-    
-    
+        var li = document.createElement("li");
+        li.textContent = addScore;
+        li.setAttribute("data-index", i);
+
+        scoreList.appendChild(li);
+    }
 }
-function displayHighScore() {
-    highScores.innerHTML = "p" + highScore;
-    
 
+
+function init() {
+    // Parsing the JSON string to an object
+    let userScores = JSON.parse(localStorage.getItem("totalScore"));
+
+    // If scores were retrieved from localStorage, update the score array to it
+    if (userScores !== null) {
+        totalScore = userScores;
+    }
+
+    setScore();
+}
+
+function storeScores() {
+    // Stringify and set "scoreArray" key in localStorage to score array
+    localStorage.setItem("totalScore", JSON.stringify(totalScore));
+    console.log(localStorage)
+}
+
+// When form is submitted...
+mainForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    scoreText = scoreEnter.value.trim();
+
+    // Return from function early if submitted scoreEnter is blank
+    if (scoreText === "") {
+        return;
+    }
+
+    // Add new scoreEnter to scoreArray array, clear the input
+    totalScore.push(scoreText + " " + finalScore);
+
+    scoreEnter.value = "";
+
+    // Store updated scores in localStorage, re-render the list
+    storeScores();
+    setScore();
+});
+function checkScore() {
+    scoreScreen.setAttribute("style", "display: inline-block, text-align: center")
+    button1.setAttribute("style", "display: none");
+    button2.setAttribute("style", "display: none");
+    button3.setAttribute("style", "display: none");
+    button4.setAttribute("style", "display: none");
+    buttonGroup.setAttribute("style", "display: none")
+    newQuest.setAttribute("style", "display: none");
+    quizBody.setAttribute("style", "display: none");
+    mainForm.setAttribute("style", "display: inline-block");
+    correct.setAttribute("style", "display: none");
+    inCorrect.setAttribute("style", "display: none");
+
+
+
+    clearInterval(timer);
+};
+
+function endGame() {
+    stopTimer();
+    scoreText;
+    startingTime = 1;
+    totalSeconds = startingTime * 60;
+    interval;
+    questionsLeft = 0;
+    score = 0;
+    finalScore;
+    totalScore = [];
+}
+
+function restartGame() {
+    endGame();
+    mainForm.setAttribute("style", "display: none");
+    newQuest.setAttribute("style", "display: none");
+    buttonGroup.setAttribute("style", "display: block")
+    quizBody.setAttribute("style", "display: inline-block");
+    //startQuiz();
 }
 
 function stopTimer() {
     clearInterval(interval);
 }
-
-
-
+// add event listeners
 startbtn.addEventListener("click", startQuiz);
 button1.addEventListener("click", nextQuestion);
 button2.addEventListener("click", nextQuestion);
 button3.addEventListener("click", nextQuestion);
 button4.addEventListener("click", nextQuestion);
-highScores.addEventListener("click", displayHighScore)
-// function for getting wrong //
+highScores.addEventListener("click", checkScore);
+resetGame.addEventListener("click", restartGame);
 
-// function for correct answer
 
-//
-//let question2 = document.createElement("h1");
-//let choices1 = document.createElement("li");
+
+
+
+
